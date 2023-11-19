12ba53bc2b39055c1944c46d3debf8998b1e7d6c
Marchello <129538566+Marchello0509@users.noreply.github.com>
2023-11-19 22:27:13 +0200

    Login & Register

--- js/script.js
+++ js/script.js
@@ -4,10 +4,27 @@ const htmlEl = {
     btnCancel: document.querySelector('#btnCancel'),
     modal: document.querySelector('#modal'),
     btnModal2: document.querySelector('#btnModal2'),
-    btnLogin2: document.querySelector('#btnLogin2'),
+    btnReg: document.querySelector('#btnReg'),
     btnCancel2: document.querySelector('#btnCancel2'),
-    modal2: document.querySelector('#modal2')
+    modal2: document.querySelector('#modal2'),
+    inpEmail: document.querySelector('#inpEmail'),
+    inpPassword: document.querySelector('#inpPassword'),
+    emailNew: document.querySelector('#emailNew'),
+    passNew: document.querySelector('#passNew'),
+    passNewVer: document.querySelector('#passNewVer'),
 }
+
+const dataBase = [
+    {
+        email: "marchello0509@gmail.com",
+        password: "Marchello0509"
+    },
+    {
+        emailReg: "marchello0509@gmail.com",
+        passwordReg: "Marchello0509",
+        passwordRegVer: "Marchello0509",
+    }
+]
 
 htmlEl.btnModal.addEventListener('click', () => {
     htmlEl.modal.style.display = "flex"
@@ -23,4 +40,31 @@ htmlEl.btnModal2.addEventListener('click', () => {
 
 htmlEl.btnCancel2.addEventListener('click', () => {
     htmlEl.modal2.style.display = "none"
+})
+
+htmlEl.btnLogin.addEventListener('click', () => {
+    const userEmail = htmlEl.inpEmail.value
+    const userPass = htmlEl.inpPassword.value
+    dataBase.forEach((user) => {
+        if (userEmail === user.email && userPass === user.password) {
+            console.log(true)
+            window.location.href = './account.html'
+        } else {
+            console.log(false)
+        }
+    })
+})
+
+htmlEl.btnReg.addEventListener('click', () => {
+    const userEmailNew = htmlEl.emailNew.value
+    const userPassNew = htmlEl.passNew.value
+    const userPassNewVer = htmlEl.passNewVer.value
+    dataBase.forEach((newUser) => {
+        if (userEmailNew === newUser.emailReg && userPassNew === newUser.passwordReg && userPassNewVer === newUser.passwordRegVer) {
+            console.log(true)
+            window.location.href = './account.html'
+        } else {
+            console.log(false)
+        }
+    })
 })
